12f1b9455ebaa2f72408d46d4144e66d35ecfdd6
pentogono <ilg3109@gmail.com>
2018-11-22 13:26:50 +0300

    variable declaration closer to use

--- src/main/java/ru/itpark/CashbackService.java
+++ src/main/java/ru/itpark/CashbackService.java
@@ -2,14 +2,13 @@ package ru.itpark;
 
 public class CashbackService {
     public int calculateCashBack(int ordinaryPurchase, int increasedPurchase, int specialOffer) {
-        int totalCashBack;
         int ordinaryCashBack = 1;
         int increatedCashBack = 5;
         int specisalCashBack = 30;
         int hundredPercent = 100;
         int limitCashBack = 3_000;
 
-        totalCashBack = ((ordinaryPurchase / hundredPercent) * ordinaryCashBack) +
+        int totalCashBack = ((ordinaryPurchase / hundredPercent) * ordinaryCashBack) +
                 ((increasedPurchase / hundredPercent) * increatedCashBack) +
                 ((specialOffer / hundredPercent) * specisalCashBack);
         if (totalCashBack > limitCashBack) {
