072fe9557370bb1d06615a995ab84ce3f57a839b
pentogono <ilg3109@gmail.com>
2018-11-30 11:36:06 +0300

    исправление орфографических ошибок

--- src/main/java/ru/itpark/CashbackService.java
+++ src/main/java/ru/itpark/CashbackService.java
@@ -3,14 +3,14 @@ package ru.itpark;
 public class CashbackService {
     public int calculateCashBack(int ordinaryPurchase, int increasedPurchase, int specialOffer) {
         int ordinaryCashBack = 1;
-        int increatedCashBack = 5;
-        int specisalCashBack = 30;
+        int increasedCashBack = 5;
+        int specialCashBack = 30;
         int hundredPercent = 100;
         int limitCashBack = 3_000;
 
         int totalCashBack = ((ordinaryPurchase / hundredPercent) * ordinaryCashBack) +
-                ((increasedPurchase / hundredPercent) * increatedCashBack) +
-                ((specialOffer / hundredPercent) * specisalCashBack);
+                ((increasedPurchase / hundredPercent) * increasedCashBack) +
+                ((specialOffer / hundredPercent) * specialCashBack);
         if (totalCashBack > limitCashBack) {
             totalCashBack = limitCashBack;
         }
